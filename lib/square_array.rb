07cec18d5d00331678243d93e36fdcad99b93037
Michael Schneider <mic.sch91@gmail.com>
2020-06-02 01:19:19 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,2 @@
-def square_array(numbers)
+def square_array(array)
   # your code here
-# square_array squares elements of array and returns to new_numbers
-numbers = [1, 2, 3]
-while square_array[numbers] do
-  puts numbers * numbers
-end
